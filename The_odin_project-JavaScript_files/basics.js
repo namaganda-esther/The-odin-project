8d55f64ba0d05f27a048d54b0ffc8ae11441f685
Esther Namaganda <45708184+namaganda-esther@users.noreply.github.com>
2023-02-06 16:58:42 +0300

    Add files via upload

---
+++ The_odin_project-JavaScript_files/basics.js
@@ -0,0 +1,35 @@
+let message;
+message = "hello";
+console.log(message);
+alert(message);
+
+let greeting = "Good Evening";
+alert(greeting);
+console.log(greeting);
+
+
+let name = "esther", number = 30, sex= "F";
+let user = "John";
+let age = 30;
+
+let hello = "hellow World";
+let hello_Two;
+
+hello_two = hello;
+console.log(hello_two);
+console.log(hello);
+
+num = 5;
+console.log(num); //if this statement is not included, it retirns an error
+
+// tasks
+let admin; 
+let names;
+names = "John";
+admin = names;
+console.log(admin);
+alert(admin);
+
+
+let Earth;
+let Current_Visitor;
